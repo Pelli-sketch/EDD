59b43259adfbf39221d19859de1b31a3e279d3ab
Pablo Pelliccioni <73903709+Pelli-sketch@users.noreply.github.com>
2024-10-20 16:33:50 -0400

    Update 1.1

---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mycompany.cobertura_de_sucursales","l":"GestorRedesTransporte"},{"p":"com.mycompany.cobertura_de_sucursales","l":"Main"}];updateSearchResults();
